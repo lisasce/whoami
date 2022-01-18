48fec25c4cefcab7650f97978bf1e742ab30d369
lisa scelli <lisa.scelli@adesso.at>
2022-01-18 12:00:33 +0100

    fix responsiveness and css of me component

--- src/app/me/me.component.ts
+++ src/app/me/me.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import {Subject, takeUntil} from "rxjs";
 
 @Component({
   selector: 'who-me',
   templateUrl: './me.component.html',
   styleUrls: ['./me.component.scss']
 })
-export class MeComponent implements OnInit {
+export class MeComponent implements OnInit, OnDestroy  {
+  destroyed = new Subject<void>();
 
-  constructor() { }
+  constructor(private breakpointObserver: BreakpointObserver) {
+
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed.next();
+    this.destroyed.complete();
+    }
 
   ngOnInit(): void {
+    this.breakpointObserver.observe([Breakpoints.XSmall,Breakpoints.Small])
+      .pipe(takeUntil(this.destroyed))
+      .subscribe(result => {
+        this.hidePolaroidOnSmallScreen(result.matches)
+      });
   }
+
+
+  hidePolaroidOnSmallScreen(isSmall: boolean){
+    if (!isSmall){
+      document.getElementById('polaroidRight')?.classList.remove('hide');
+      document.getElementById('polaroidRight')?.classList.add('show');
+    }
+    else if (isSmall){
+      document.getElementById('polaroidRight')?.classList.remove('show');
+      document.getElementById('polaroidRight')?.classList.add('hide');
+    }
+  }
+
 
 }
