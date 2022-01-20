bc43ee68136b520c3a4e8f0be1c0d880af7d2e25
lisa scelli <lisa.scelli@adesso.at>
2022-01-20 16:50:29 +0100

    implement thesis component

--- src/app/theses/theses.component.ts
+++ src/app/theses/theses.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
+import {Subject, takeUntil} from "rxjs";
 
 @Component({
   selector: 'who-theses',
   templateUrl: './theses.component.html',
   styleUrls: ['./theses.component.scss']
 })
-export class ThesesComponent implements OnInit {
+export class ThesesComponent implements OnInit, OnDestroy {
+  panel1OpenState = false;
+  panel2OpenState = false;
+  destroyed = new Subject<void>();
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
-  constructor() { }
+  ngOnDestroy(): void {
+    this.destroyed.next();
+    this.destroyed.complete();
+  }
 
   ngOnInit(): void {
+    this.breakpointObserver.observe([Breakpoints.XSmall, Breakpoints.Small])
+      .pipe(takeUntil(this.destroyed))
+      .subscribe(result => {
+        this.hidePolaroidOnSmallScreen(result.matches)
+      });
+  }
+
+
+  hidePolaroidOnSmallScreen(isSmall: boolean) {
+    if (!isSmall) {
+      document.getElementById('polaroidLeft')?.classList.remove('hide');
+    } else if (isSmall) {
+
+      document.getElementById('polaroidLeft')?.classList.add('hide');
+    }
   }
 
 }
